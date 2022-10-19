8e304625ee3b6b8eba06c26b5cccec8846d4160c
pratas <6882743+pratas@users.noreply.github.com>
2022-10-19 11:17:56 +0100

    denovo link

--- src/RunAllDeNovo.sh
+++ src/RunAllDeNovo.sh
@@ -2,7 +2,7 @@
 #
 # ==============================================================================
 #
-lzma -k -d VDB.fa.lzma
+lzma -k -f -d VDB.fa.lzma
 #
 ls *_forward.fq.gz > .r1_names.txt;
 ls *_reverse.fq.gz > .r2_names.txt;
@@ -18,7 +18,7 @@ for reads in "${READS_NAMES_1[@]}"
   R2=`echo ${READS_NAMES_2[$idx]} | awk '{ print $1 }'`;
   echo "Running $R1 and $R2 ...";
   #
-  ./TRACESPipeLite.sh --similarity 20 --reads1 $R1 --reads2 $R2 --database VDB.fa --threads 16 --output results_denovo_R-$R1-$R2-ready
+  ./TRACESPipeDeNovo.sh --similarity 20 --reads1 $R1 --reads2 $R2 --database VDB.fa --threads 16 --output results_denovo_R-$R1-$R2-ready
   #
   echo "Done!";
   ((++idx));
